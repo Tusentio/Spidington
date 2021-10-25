8ed8e63fe4d7d721a51012fca83d155f4d5ede50
Kevin Sundqvist Norlén <killenlillen02@gmail.com>
2021-10-25 10:42:11 +0200

    Rename "String" to "Spurt" and adjust string behavior

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 
-const string_shot := preload("res://Scenes/String/String.tscn")
+const scn_spurt := preload("res://Scenes/Spurt/Spurt.tscn")
 
 export (float) var string_speed := 500.0
 onready var shoot_origin := $ShootOrigin
 var _is_ready := false
-var shot: StringShot = null
+var spurt: Spurt = null
 
 
 func _ready():
@@ -15,22 +15,22 @@ func _ready():
 
 func _input(event):
 	if Input.is_action_just_pressed("shoot_string"):
-		if shot:
-			shot.disconnect("hit", self, "_shot_hit")
-			shot.detach()
+		if is_instance_valid(spurt):
+			spurt.disconnect("hit", self, "_shot_hit")
+			spurt.detach()
 		
 		var mouse := get_global_mouse_position()
 		var shoot_direction: Vector2 = (mouse - shoot_origin.global_position).normalized()
 		
-		shot = string_shot.instance()
+		spurt = scn_spurt.instance()
 		
-		shot.global_position = shoot_origin.global_position
-		shot.set_velocity(shoot_direction * 1)
-		shot.set_anchor(shoot_origin)
+		spurt.global_position = shoot_origin.global_position
+		spurt.set_velocity(shoot_direction * 10)
+		spurt.set_anchor(shoot_origin)
 		
-		shot.connect("hit", self, "_shot_hit")
+		spurt.connect("hit", self, "_shot_hit")
 		
-		get_tree().get_root().add_child(shot)
+		get_tree().get_root().add_child(spurt)
 
 func _shot_hit():
-	shot.detach()
+	spurt.detach()
--- Scenes/Spurt/Spurt.gd
+++ Scenes/Spurt/Spurt.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-class_name StringShot
+class_name Spurt
 
 
 signal detached
@@ -7,8 +7,10 @@ signal hit
 
 
 const GRAVITY := 1000.0
-const DRAG := 0.5
+const DRAG := 0.001
 const SHORTENING_COEFFICIENT := 0.8
+const MIN_STRING_LENGTH := 1.0
+const MAX_STRING_LENGTH := 100.0
 
 var anchor: Node2D = null
 var velocity := Vector2.ZERO
@@ -17,16 +19,26 @@ onready var line := $Line2D
 
 
 func _process(delta):
-	if anchor:
+	var string_length = line.points[1].length()
+	
+	if is_instance_valid(anchor):
+		if string_length > MAX_STRING_LENGTH:
+			detach()
+			return
+		
 		var space_state := get_world_2d().direct_space_state
 		var obstruction := space_state.intersect_ray(global_position, anchor.global_position, [], 0b10)
 		if obstruction:
 			detach()
+			return
 		
 		line.points[1] = to_local(anchor.global_position)
 	else:
-		var string_length = line.points[1].length()
 		string_length *= pow(SHORTENING_COEFFICIENT, delta)
+		
+		if string_length < MIN_STRING_LENGTH:
+			queue_free()
+			return
 		
 		# Gravity
 		tail_velocity += Vector2.DOWN * delta * GRAVITY
@@ -38,21 +50,21 @@ func _process(delta):
 		tail_velocity = (line.points[1] - _pos) / delta
 		
 		# Drag
-		tail_velocity += -tail_velocity.normalized() * tail_velocity.length_squared() * delta * DRAG
+		var wind_velocity = tail_velocity + velocity
+		tail_velocity += -wind_velocity.normalized() * wind_velocity.length_squared() * delta * string_length * DRAG
 
 
 func _physics_process(delta):
 	if velocity:
 		var collision := move_and_collide(velocity)
 		
-		if collision:
+		if is_instance_valid(anchor) and collision:
 			velocity = Vector2.ZERO
 			emit_signal("hit")
 
 
 func set_velocity(velocity):
 	self.velocity = velocity
-	tail_velocity = velocity
 
 
 func set_anchor(anchor: Node2D):
