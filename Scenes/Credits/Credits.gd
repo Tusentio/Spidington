271f2e085e31c3be821c2e65a07846f2996349ed
Keve1227 <killenlillen02@gmail.com>
2021-10-26 17:13:18 +0200

    Update and animate credits text

--- Scenes/Credits/Credits.gd
+++ Scenes/Credits/Credits.gd
@@ -1,5 +1,16 @@
 extends Control
 
+export (float) var text_speed := 0.0
+export (float) var text_acceleration := 2.0
+
 func _input(event):
 	if Input.is_action_just_pressed("pause_menu"):
 		get_tree().change_scene("res://Scenes/Menu/Menu.tscn")
+
+func _ready():
+	$Content/Label.percent_visible = 0
+
+func _process(delta):
+	if $Content/Label.percent_visible < 1:
+		$Content/Label.percent_visible += text_speed * delta
+		text_speed += text_acceleration * delta
