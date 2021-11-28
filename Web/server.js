93aa93d5970ea94285ae34b364adea95e64832da
Keve1227 <killenlillen02@gmail.com>
2021-11-28 16:00:58 +0100

    Semantic versioning

--- Web/server.js
+++ Web/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const fs = require("fs");
 const path = require("path");
+const semver = require("semver");
 const express = require("express");
 const config = require("./config.json");
 
@@ -9,10 +10,24 @@ const analyticsLog = [];
 
 const app = express();
 
+// Version checking
+app.use((req, res, next) => {
+    const version = semver.valid(req.headers["spidington"]);
+
+    if (!version) {
+        return res.sendStatus(400);
+    }
+
+    if (!semver.satisfies(version, config.versionSupport)) {
+        return res.sendStatus(403);
+    }
+
+    return next();
+});
+
 app.use(express.json());
 
-app.use((_err, _req, res, _next) => res.sendStatus(400));
-
+// Analytics
 app.post("/", (req, res) => {
     let dur, evq, iat, sid;
 
@@ -28,6 +43,9 @@ app.post("/", (req, res) => {
 
     return res.sendStatus(200);
 });
+
+// Error handling
+app.use((_err, _req, res, _next) => res.sendStatus(400));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}.`);
