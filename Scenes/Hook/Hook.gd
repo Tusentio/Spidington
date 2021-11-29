ea0975a14bab212bbd30326b392c001042630bd5
Octaphous <pappkasse2121@gmail.com>
2021-11-29 01:52:50 +0100

    Controversial hook update

--- Scenes/Hook/Hook.gd
+++ Scenes/Hook/Hook.gd
@@ -1,11 +1,14 @@
 extends StaticBody2D
 class_name Hook
 
-
 func anchored():
+	$Indicator.visible = false;
 	$HitSound.play()
-	$Animator.play("Anchored");
-
+	$Hook.get_node("Animator").play("Anchored");
+	pass;
 
 func unanchored():
-	pass
+	$Indicator.visible = true;
+	$Hook.get_node("Animator").stop();
+	$Hook.get_node("Animator").seek(0, true);
+	pass;
