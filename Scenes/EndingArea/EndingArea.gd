c251dd829d1ddebe58dab358c806f2d0ca4d102f
Keve1227 <killenlillen02@gmail.com>
2021-11-30 23:26:11 +0100

    Better analytics

--- Scenes/EndingArea/EndingArea.gd
+++ Scenes/EndingArea/EndingArea.gd
@@ -3,5 +3,6 @@ extends Area2D
 var reset := false
 
 
-func _on_EndingArea_body_entered(body):	
-	SceneChanger.change_scene(SceneChanger.CREDITS);
+func _on_EndingArea_body_entered(body):
+	AnalyticsCollector.send_event("the_end")
+	SceneChanger.change_scene(SceneChanger.CREDITS)
