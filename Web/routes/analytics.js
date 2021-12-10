1d024baf99ac5f616ef0b8542a21e6e5b39325c6
Kevin Sundqvist Norlén <killenlillen02@gmail.com>
2021-12-10 10:42:28 +0100

    Use JSON Schema to check analytics packet validity

--- Web/routes/analytics.js
+++ Web/routes/analytics.js
@@ -3,7 +3,10 @@ const path = require("path");
 const semver = require("semver");
 const sanitize = require("sanitize-filename");
 const express = require("express");
+const Ajv = require("ajv");
 const config = require("../config.json");
+
+const validatePacket = new Ajv().compile(require("../schemas/analytics-packet.json"));
 
 const router = express.Router();
 
@@ -26,12 +29,14 @@ router.use((req, res, next) => {
 
 router.use(express.json());
 
+// Analytics logging
 router.post("/", (req, res) => {
-    if (!req.body.evq || !req.body.sid) {
+    const entry = req.body;
+
+    if (!validatePacket(entry)) {
         return res.sendStatus(400);
     }
 
-    const entry = req.body;
     log.push(entry);
 
     if (config.analytics.echo) {
@@ -41,18 +46,18 @@ router.post("/", (req, res) => {
     return res.sendStatus(200);
 });
 
-module.exports = {
-    router,
-    async save() {
-        const directory = config.analytics.directory;
-        await fs.promises.mkdir(directory, { recursive: true });
+async function save() {
+    const directory = config.analytics.directory;
+    await fs.promises.mkdir(directory, { recursive: true }); // Ensure directory
 
-        if (log.length > 0) {
-            const file = path.resolve(directory, sanitize(new Date().toISOString()) + ".json");
-            await fs.promises.writeFile(file, JSON.stringify(log));
+    // Save new log file if there are unsaved entries
+    if (log.length > 0) {
+        const file = path.resolve(directory, sanitize(new Date().toISOString()) + ".json");
+        await fs.promises.writeFile(file, JSON.stringify(log));
 
-            // Clear log
-            log.splice(0);
-        }
-    },
-};
+        // Clear log
+        log.splice(0);
+    }
+}
+
+module.exports = { router, save };
